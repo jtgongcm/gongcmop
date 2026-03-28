4f45d056c3e79e4662aaef474c1432b6ae0394ea
jtgongcm <jtgongcm@gmail.com>
2026-03-28 10:09:36 +0800

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 修改默认IP为192.168.1.1
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认主题为Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修正AdGuardHome架构检测
+sed -i 's/ /_/g' feeds/packages/net/adguardhome/Makefile
